7388fce0782963b65ef6f99010e9911176e24892
Daniel Harman <danielgharman@outlook.com>
2022-11-22 23:31:33 +0000

    It connects but not much else

--- Dotbot/Program.cs
+++ Dotbot/Program.cs
@@ -1,4 +1,5 @@
-var builder = WebApplication.CreateBuilder(args);
+using Discord;
+using Discord.WebSocket;var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -6,6 +7,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var discordConfig = new DiscordSocketConfig()
+{
+    GatewayIntents = GatewayIntents.AllUnprivileged
+};
+
+builder.Services.AddSingleton(discordConfig);
+builder.Services.AddSingleton<DiscordSocketClient>();
+
 
 var app = builder.Build();
 
@@ -22,4 +32,22 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+var client = app.Services.GetRequiredService<DiscordSocketClient>();
+
+client.Log += async (msg) =>
+{
+    await Task.CompletedTask;
+    Console.WriteLine(msg.Message);
+};
+
+client.MessageReceived += async msg =>
+{
+    await Task.CompletedTask;
+    Console.WriteLine(msg);
+};
+
+await client.LoginAsync(TokenType.Bot, "");
+await client.StartAsync();
 app.Run();
+
+//await Task.Delay(Timeout.Infinite);
