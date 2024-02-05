047749556c94f3d3ca1eccb584ec33aebc5efa9e
sillock1 <jprest97@gmail.com>
2024-02-05 19:28:13 +0000

    chore: added tooling for local cluster development

--- Tiltfile
+++ Tiltfile
@@ -4,10 +4,10 @@ if k8s_context() != 'kind-kind':
   fail("failing early to avoid overwriting prod")
 
 load('ext://helm_remote', 'helm_remote')
-helm_remote('mongodb', repo_name='bitnami', repo_url='https://charts.bitnami.com/bitnami', values=['./localdev/k8s/mongodb-values.yaml'])
-helm_remote('rabbitmq', repo_name='bitnami', repo_url='https://charts.bitnami.com/bitnami', values=['./localdev/k8s/rabbitmq-values.yaml'])
-helm_remote('localstack', repo_name='localstack', repo_url='https://localstack.github.io/helm-charts')
+helm_remote('mongodb', repo_name='bitnami', repo_url='https://charts.bitnami.com/bitnami', set=['auth.rootPassword=root', 'architecture=replicaset'])
+helm_remote('rabbitmq', repo_name='bitnami', repo_url='https://charts.bitnami.com/bitnami', set=['auth.username=guest', 'auth.password=guest'])
 
+helm_remote('localstack', repo_name='localstack', repo_url='https://localstack.github.io/helm-charts')
 k8s_yaml(helm('charts/dotbot', 'dotbot', values=['./charts/dotbot/values.yaml', './localdev/k8s/dotbot-values.yaml']))
 docker_build('gateway', context='.', dockerfile='./src/Bot.Gateway/Dockerfile')
 docker_build('xkcd-job', context='.', dockerfile='./src/Xkcd.Job/Dockerfile')
--- localdev/k8s/bootstrap.sh
+++ localdev/k8s/bootstrap.sh
@@ -17,22 +17,52 @@ kind create cluster
 kubectl config use-context kind-kind
 
 if [[ -z "$BOTTOKEN" ]]; then 
-  echo "Discord Bot token not found, please ensure you have $BOTTOKEN" >> /dev/stderr
+  echo "Discord Bot token not found, please ensure you have "$BOTTOKEN"" >> /dev/stderr
 exit 1
 fi
 
 if [[ -z "$BOTPUBKEY" ]]; then 
-  echo "Discord Bot public key not found, please ensure you have $BOTPUBKEY" >> /dev/stderr
+  echo "Discord Bot public key not found, please ensure you have "$BOTPUBKEY"" >> /dev/stderr
+exit 1
+fi
+
+if [[ -z "$WEBHOOKURL" ]]; then
+  echo "Webhook not found for xkcd channel, please ensure you have "$WEBHOOKURL"" >> /dev/stderr
+exit 1
+fi
+
+if [[ -z "$NGROK_API_KEY" ]]; then
+  echo "ngrok API key is required "$NGROK_API_KEY"" >> /dev/stderr
+exit 1
+fi
+
+if [[ -z "$NGROK_AUTHTOKEN" ]]; then
+  echo "ngrok auth token is required "$NGROK_AUTHTOKEN"" >> /dev/stderr
 exit 1
 fi
 
 kubectl create secret generic dotbot-secret \
+  --from-literal=DiscordSettings__Webhooks__xkcd=$WEBHOOKURL \
   --from-literal=DiscordSettings__BotToken=$BOTTOKEN \
   --from-literal=DiscordSettings__PublicKey=$BOTPUBKEY \
   --from-literal=MongoDbSettings__DatabaseName="localdev" \
-  --from-literal=MongoDbSettings__ConnectionString="mongodb://root:root@mongo:27017/?authMechanism=DEFAULT" \
-  --from-literal=RabbitMQ__Endpoint="rabbitmq" \
+  --from-literal=MongoDbSettings__ConnectionString="mongodb+srv://root:root@mongodb-headless.default.svc.cluster.local?tls=false&ssl=false&replicaSet=rs0" \
+  --from-literal=RabbitMQ__Endpoint="rabbitmq.default.svc.cluster.local" \
   --from-literal=RabbitMQ__Username="guest" \
   --from-literal=RabbitMQ__Password="guest" \
   --from-literal=AWS_PROFILE="localdev" \
-  --from-literal=localdev__ServiceURL="http://localstack:4566"
+  --from-literal=S3__Profile="localdev" \
+  --from-literal=S3__ServiceURL="http://localstack:4566" \
+  --from-literal=S3__ForcePathStyle=true \
+  --from-literal=AWS_ACCESS_KEY_ID='localdev' \
+  --from-literal=AWS_SECRET_KEY='localdev' 
+
+helm install ngrok-ingress-controller ngrok/kubernetes-ingress-controller \
+  --namespace ngrok-ingress-controller \
+  --create-namespace \
+  --set credentials.apiKey=$NGROK_API_KEY \
+  --set credentials.authtoken=$NGROK_AUTHTOKEN
+
+kubectl create ingress dotbot-ingress \
+  --class=ngrok \
+  --rule=$NGROK_DOMAIN/*=dotbot:80
