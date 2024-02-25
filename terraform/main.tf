0ce1a34afe9a32a6623e4229809a7e404f17e863
sillock1 <jprest97@gmail.com>
2024-02-25 01:10:09 +0000

    chore: removed unused terraform config

--- terraform/main.tf
+++
@@ -1,8 +0,0 @@
-module "cloudflare_tunnel" {
-  source = "github.com/Sillock-Inc/terraform-cloudflare-tunnel-module?ref=v0.1.1"
-
-  cloudflare_account_id = var.cloudflare_account_id
-  tunnel_name           = var.tunnel_name
-  ingress_rules         = var.ingress_rules
-  zone_dns              = var.zone_dns
-}
